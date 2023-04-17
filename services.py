911fc2c063480aadae22ab645d69ab3ccf0684dc
Tom Schneider <schneider.thomas.g@gmail.com>
2023-04-16 22:15:46 -0800

    Added comments

--- services.py
+++ services.py
@@ -2,17 +2,12 @@ from abc import ABC, abstractmethod
 
 from shapely import Geometry
 
-import constants
 from entities import *
 
 class IArea(ABC):
     """
     Abstract class to serve as an interface for area sources.
 
-    Methods:
-    --------
-    extract_geometry()
-        Extracts a list of points which form the boundary of an area.
     """
 
     @abstractmethod
@@ -33,11 +28,6 @@ class IStorm(ABC):
     """
     Abstract class to serve as an interface for track data sources.
 
-    Methods:
-    --------
-    extract_data()
-        Extracts the data from the given source and returns a list of Track objects.
-
     """
 
     @abstractmethod
@@ -50,9 +40,9 @@ class AreaService:
 
     Attributes:
     -----------
-        name:       str
-            Name of the area
-        shape:      shaprely.Geometry
+        repository: IArea
+            Instance of the IArea repository base class, used for ingesting area data.
+        shape:      shapely.Geometry
             Geometry instance object.
 
     Methods:
@@ -72,17 +62,22 @@ class StormDataService:
 
     Attributes:
     -----------
+    repo:   IStorm
+        Instance of the IStorm interface base class. Used to ingest storm data.
+    area:   IArea
+        Instance of the IArea interface base class. Used to hold data and methods for the area being analyzed.
     tracks: List[Track]
-        A list of Track instances.
+        A list of Track instances for the storm data ingested.
 
-    Methods:
-    --------
-    makes_landfall(self, area)
-        Returns whether the storm track contains any points which are within area.
+    Static Methods:
+    ---------------
+    get_landfall_dates(track, area)
+        Returns all dates and times when the storm makes landfall over an area, with the max windspeed at that time.
 
-    get_landfall_dates(self, area)
-        Returns a list of TrackEntry instances where the storm has made landfall.
-        A storm is considered to make landfall multiple times when at least 1 TrackEntry outside area falls between
+    Public Methods:
+    ---------------
+    record_landfall_dates(self)
+        Returns a list of dict object containing the track name and a list of all landfall times and wind speeds.
 
     
     get_max_windspeed()
@@ -93,7 +88,46 @@ class StormDataService:
         self.area = area
         self.tracks = self.repo.extract_data()
 
-    def record_landfall_events(self):
+    @staticmethod
+    def get_landfall_dates(track: Track, area: Geometry) -> List[TrackEntry]:
+        """
+        
+        Returns all dates and times when the storm makes landfall over an area, with the max windspeed at that time.
+
+        Parameters:
+        -----------
+        track: Track
+            Track object of the storm being analyzed.
+        area: Area
+            The area where we want to identify landfall events.
+
+        Returns:
+        --------
+            List of TrackEntry instances.
+        
+        """
+        in_area = False
+        entries = []
+        for track_entry in track.track_entries:
+            # Check if this point is in the area specified, and has a hurricane designation.
+            if not (track_entry.system_status == 'HU' and area.contains(track_entry.location.to_point())):
+                in_area = False
+                continue
+            # If the in_area flag is True, then this is not a new landfall event: the storm has not yet moved out of the target area.
+            if in_area:
+                continue
+
+            # Append the new landfall event to entries.
+            else:                
+                in_area = True
+                entries.append(track_entry)
+
+        return entries
+
+    def record_landfall_events(self) -> List[dict]:
+        """
+        Returns a list of dict object containing the track name and a list of all landfall times and wind speeds.
+        """
         records = []
 
         for track in self.tracks:
@@ -108,40 +142,7 @@ class StormDataService:
         
         return records
 
-    @staticmethod
-    def get_landfall_dates(track: Track, area: Geometry) -> List[TrackEntry]:
-        """
-        
-        Returns all dates and times when the storm makes landfall over an area, 
-        with the max windspeed at that time.
-
-        Parameters:
-        -----------
-        area: Area
-            The land where we want to identify landfall events.
-
-        Returns:
-        --------
-            List of TrackEntry instances.
-        
-        """
-        in_area = False
-        entries = []
-        for track_entry in track.track_entries:
-            # Check if this point is in the area specified.
-            if not area.contains(track_entry.location.to_point()):
-                in_area = False
-                continue
-            # If the in_area flag is True, then this is not a new landfall event: the storm has not yet moved out of the target area.
-            if in_area:
-                continue
-
-            # Append the new landfall event to entries.
-            else:                
-                in_area = True
-                entries.append(track_entry)
-
-        return entries
+    
 
 
 class ReportService():
@@ -156,21 +157,44 @@ class ReportService():
     report_date: str
         The date the report was made, in the format {YYYY}{MM}{DD}
 
-    Methods:
+    Public Methods:
     --------
-    create_report(fields:)
+    generate(fields):
+
+
     """
 
-    def __init__(self, repo) -> None:
+    def __init__(self, repo: IReport) -> None:
         self.report = {}
         self.report_date = datetime.today()
         self.repository = repo()
 
-    def generate(self, storms):
+    def generate(self, storms) -> None:
+        """
+        Adds a report date to the storms parameter and saves them to the instance for saving.
+
+        Paramters:
+        ----------
+        storms: List[dict]
+            A list of dicts containing information about storms to be added to the report.
+
+        """
         self.report = {
             'Report Date': str(self.report_date),
             'Storms making landfall:': storms,
         }
 
-    def save(self, target: str):
-        self.repository.save(target, self.report)
+    def save(self, target: str) -> int:
+        """"
+        
+        Saves the report attribute using the provided IReport repository.
+        Returns a success (200) or error (400) code.
+
+        Parameters:
+        -----------
+        target: str
+            String of the relative target directory path.
+
+        """
+        return_code = self.repository.save(target, self.report)
+        return return_code
